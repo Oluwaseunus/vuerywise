84f4c04a76251c9fb4223ed271360a1eddbd055c
Oluwaseunus <oluwaseunus@gmail.com>
2019-07-24 13:42:27 +0100

    Show African Pictures on load and setup scss

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,73 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="search-container">
+      <form @submit.prevent="handleSubmit">
+        <input v-model="search" type="text" placeholder="Search for photo" class="search-box_input" />
+      </form>
+    </div>
+    <div class="pictures-container" v-if="photos.length">
+      <PhotoGridItem v-for="photo in photos" :key="photo.id" :photo="photo" />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import PhotoGridItem from "./components/PhotoGridItem";
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+  export default {
+    components: {
+      PhotoGridItem
+    },
+    data() {
+      return {
+        photos: [],
+        search: ""
+      };
+    },
+    methods: {
+      handleSubmit() {
+        console.log(this.search);
+      }
+    },
+    mounted() {
+      fetch(
+        `https://api.unsplash.com/search/photos?client_id=${process.env.VUE_APP_ACCESS}&query='African'`
+      )
+        .then(response => response.json())
+        .then(data => (this.photos = data.results))
+        .then(() => console.log(this.photos));
+    }
+  };
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+  #app {
+    margin: auto;
+  }
+
+  .search-container {
+    width: 100%;
+    height: 25vh;
+    background-color: #dde2e9;
+
+    form {
+      width: 100%;
+      height: 100%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      input {
+        border: none;
+        height: 40%;
+        width: 70%;
+        border-radius: 15px;
+        box-shadow: 0 4px 12px 0px #dde2e9;
+
+        &:focus {
+          outline: none;
+        }
+      }
+    }
+  }
 </style>
---
+++ src/components/PhotoGridItem.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="photo-grid-item" :style="style">
+    <div class="photo-grid-details">
+      <p class="photo-grid-name">{{ photo.user.name }}</p>
+      <p v-if="photo.user.location" class="photo-grid-location">{{ photo.user.location }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      photo: Object
+    },
+    computed: {
+      style() {
+        return {
+          backgroundImage: `url(${this.photo.urls.small})`
+        };
+      }
+    }
+  };
+</script>
